68933113af1d4fec57a74cb6e1051df71a6eed56
azwsx123 <549193457@qq.com>
2021-12-24 18:05:52 +0800

    Exam

---
+++ Notepad.java
@@ -0,0 +1,11 @@
+ class Notepad {
+    int id;
+    String notepadContent;
+    String notepadTime;
+
+    @Override
+    public String toString() {
+        return "Notepad [id=" + id + ", notepadContent=" + notepadContent + ", notepadTime=" + notepadTime + "]";
+    }
+}
+
